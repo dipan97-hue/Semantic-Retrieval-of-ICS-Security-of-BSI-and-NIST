ae6a7da2aa39a79be366339a799a46e598fd9f21
dipan banerjee <dipanbanerjee97@gmail.com>
2026-06-30 16:21:34 +0200

    Config file changed

--- config/config.py
+++ config/config.py
@@ -1,22 +1,26 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 import streamlit as st
 
 try:
+
     if st.runtime.exists():
 
-        supabase_url = st.secrets["SUPABASE_URL"]
-        supabase_key = st.secrets["SUPABASE_KEY"]
-        gemini_api_key = st.secrets["GEMINI_API_KEY"]
-        openai_api_key = st.secrets["OPENAI_API_KEY"]
-        anthropic_api_key = st.secrets["ANTHROPIC_API_KEY"]
-        mistral_api_key = st.secrets["MISTRAL_API_KEY"]
+        SUPABASE_URL = st.secrets["SUPABASE_URL"]
+        SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+        GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+        OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+        ANTHROPIC_API_KEY = st.secrets["ANTHROPIC_API_KEY"]
+        MISTRAL_API_KEY = st.secrets["MISTRAL_API_KEY"]
+
     else:
+
         raise Exception()
 
 except:
-
 
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
